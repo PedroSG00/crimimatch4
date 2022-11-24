b7434007e282707706f891017113013c227f44b6
Raquel Tejada de Jesus <tejadadejesusraquel@gmaile.com>
2022-11-24 15:38:37 +0100

    bootstrap, css, criminal details

--- routes/criminals.routes.js
+++ routes/criminals.routes.js
@@ -11,7 +11,6 @@ router.get('/api', (req, res, next) => {
     fetch(`https://api.fbi.gov/wanted/v1/list?page=${page}`)
         .then(res => res.json())
         .then(responseFromAPI => {
-
             const arr = responseFromAPI.items
             res.render('criminal/list', { arr })
         })
@@ -23,11 +22,10 @@ router.get('/match', (req, res, next) => {
 })
 
 router.get('/match/result', (req, res, next) => {
-    const { hair, eyes, sex } = req.query
 
-    console.log(`https://api.fbi.gov/wanted?eyes=${eyes}&hair=${hair}&sex=${sex}`)
+    const { hair, eyes, sex, race } = req.query
 
-    fetch(`https://api.fbi.gov/wanted?eyes=${eyes}&hair=${hair}&sex=${sex}`)
+    fetch(`https://api.fbi.gov/wanted?eyes=${eyes}&hair=${hair}&sex=${sex}&race=${race}`)
         .then(res => res.json())
         .then(responseFromAPI => {
 
@@ -35,8 +33,21 @@ router.get('/match/result', (req, res, next) => {
             const criminal = responseFromAPI.items[index]
             console.log(criminal)
             res.render('criminal/result', { criminal })
-
         })
+        .catch(err => console.error('ERROR', err))
+})
+
+router.get('/api/:title/details', (req, res, next) => {
+
+    const { title } = req.params
+
+    fetch(`https://api.fbi.gov/wanted/v1/list?title=${title}`)
+        .then(res => res.json())
+        .then(titleFromAPI => {
+            const oneTitle = titleFromAPI.items.filter(criminal => criminal.title === title)
+            res.render('criminal/details', oneTitle[0])
+        })
+        .catch(err => console.error('ERROR', err))
 })
 
 
